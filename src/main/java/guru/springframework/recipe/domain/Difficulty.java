abe94254ee3a401dca33f1fe5b20e99aeb56778a
Jacobanna <pieprzyk.jakub@gmail.com>
2019-05-05 22:52:25 +0200

    Working with JPA.

---
+++ src/main/java/guru/springframework/recipe/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package guru.springframework.recipe.domain;
+
+//I think it should be @Entity, not marked in tutorial
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
---
+++ src/main/java/guru/springframework/recipe/repository/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.recipe.repository;
+
+import guru.springframework.recipe.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
